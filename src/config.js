4e552a8ff7d1b5a3104d2a40b3ca4a07e743bddf
Ilmi <83828336+IrumiDesu7@users.noreply.github.com>
2022-07-19 08:12:41 +0800

    updated config as it's just a config apiKey

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: 'AIzaSyCKOOGhwjizpUfuPz_mszSSg3Mg8orfCF4',
+  authDomain: 'maglo-financial-app.firebaseapp.com',
+  projectId: 'maglo-financial-app',
+  storageBucket: 'maglo-financial-app.appspot.com',
+  messagingSenderId: '850996989984',
+  appId: '1:850996989984:web:a277639e685086763c6c45',
 };
 
 const app = initializeApp(firebaseConfig);
